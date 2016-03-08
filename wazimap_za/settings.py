52f88147cf91018ab6e95ddb4723c2a49b98a5ca
Damian <damian@code4sa.org>
2016-03-08 17:37:00 +0200

    Use new local database

--- wazimap_za/settings.py
+++ wazimap_za/settings.py
@@ -4,7 +4,7 @@ from wazimap.settings import *  # noqa
 # install this app before Wazimap
 INSTALLED_APPS = ['wazimap_za'] + INSTALLED_APPS
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://census:census@localhost/census_2011')
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://wazimap_za:wazimap_za@localhost/wazimap_za')
 DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
