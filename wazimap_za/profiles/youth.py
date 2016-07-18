43b9646bb64d99a1d35865204e80719625c68909
Damian <damian@code4sa.org>
2016-07-18 13:31:58 +0200

    Education levels

--- wazimap_za/profiles/youth.py
+++ wazimap_za/profiles/youth.py
@@ -78,12 +78,10 @@ def get_demographics_profile(geo_code, geo_level, session):
 
 
 def get_education_profile(geo_code, geo_level, session):
-    youth_completed_grade9, youth_pop_total = get_stat_data(['completed grade9'], geo_level, geo_code, session, table_name='youth_gender_completed_grade9')
+    youth_completed_grade9, _ = get_stat_data(['completed grade9'], geo_level, geo_code, session, table_name='youth_gender_completed_grade9')
 
     youth_gender_completed_grade9, _ = get_stat_data(['gender', 'completed grade9'], geo_level, geo_code, session, table_name='youth_gender_completed_grade9')
-
     db_model_gender_completed_grade9 = get_model_from_fields(['gender'], geo_level, table_name='youth_gender_completed_grade9')
-
     gender_completed_grade9_data = OrderedDict((  # census data refers to sex as gender
             ('Female', {
                 "name": "Female",
@@ -96,8 +94,15 @@ def get_education_profile(geo_code, geo_level, session):
                 "numerators": {"this": youth_gender_completed_grade9['Male']['Yes']['numerators']['this']},
             }),
         ))
-
     add_metadata(gender_completed_grade9_data, db_model_gender_completed_grade9)
+
+    youth_education_level, youth_pop_20_to_24 = get_stat_data(['education level'], geo_level, geo_code, session, table_name='youth_age_20_to_24_gender_education_level')
+
+    matric_or_equiv = (
+        youth_education_level['Matric']['numerators']['this'] +
+        youth_education_level['Tertiary']['numerators']['this'] +
+        youth_education_level['Some secondary']['numerators']['this'])
+
 
     final_data  = {
         'youth_completed_grade9': youth_completed_grade9,
@@ -105,7 +110,12 @@ def get_education_profile(geo_code, geo_level, session):
             "name": "Of youth aged 16-17 have completed grade 9",
             "values": {"this": youth_completed_grade9['Yes']['values']['this']},
         },
-        'youth_gender_completed_grade9': gender_completed_grade9_data
+        'youth_gender_completed_grade9': gender_completed_grade9_data,
+        'youth_perc_matric': {
+            "name": "Of youth aged 20-24 have completed matric or matric equivalent",
+            "values": {"this": percent(matric_or_equiv, youth_pop_20_to_24)},
+        },
+        'youth_education_level': youth_education_level
     }
 
     return final_data
