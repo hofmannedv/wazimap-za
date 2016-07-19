7bd397feed067645d45ca9e5bc9772577728c9cd
Damian <damian@code4sa.org>
2016-07-19 16:08:54 +0200

    Restructure into health, living environment and economic opportunities

--- wazimap_za/profiles/youth.py
+++ wazimap_za/profiles/youth.py
@@ -8,7 +8,9 @@ from wazimap.geo import geo_data
 PROFILE_SECTIONS = (
     "demographics",
     "education",
-    "depravation"
+    "health",
+    "living_environment",
+    "economic_opportunities"
 )
 
 def get_profile(geo_code, geo_level, profile_name=None):
@@ -121,15 +123,29 @@ def get_education_profile(geo_code, geo_level, session):
     return final_data
 
 
-def get_depravation_profile(geo_code, geo_level, session):
+def get_health_profile(geo_code, geo_level, session):
     table = get_datatable('youth').model
-    (youth_pop, youth_prop, edu_dep, disab_dep, light_dep, heat_dep,
-    cook_dep, toilet_dep, water_dep, dwell_dep, asset_dep, emp_dep,
-    neets_dep, prop_multid_poor, youth_mpi) = session.query(
-            table.c.youth_pop,
-            table.c.youth_proportion,
-            table.c.edu_dep,
-            table.c.disab_dep,
+    disab_dep = session.query(
+            table.c.disab_dep) \
+        .filter(table.c.geo_level == geo_level) \
+        .filter(table.c.geo_code == geo_code) \
+        .one()[0]
+
+    final_data = {
+        'disab_dep': {
+            "name": "Proportion of youth experiencing difficulty in one or more of the following functions: hearing, vision, communication, mobility, cognition and self-care",
+            "values": {"this": float(disab_dep) or 0.0},
+            }
+    }
+
+    return final_data
+
+def get_living_environment_profile(geo_code, geo_level, session):
+    table = get_datatable('youth').model
+
+    (lighting_dep, heating_dep, cooking_dep, toilet_dep, water_dep,
+     dwell_dep, asset_dep, emp_dep, neets_dep, prop_multid_poor,
+     youth_mpi) = session.query(
             table.c.light_dep,
             table.c.heat_dep,
             table.c.cook_dep,
@@ -145,62 +161,62 @@ def get_depravation_profile(geo_code, geo_level, session):
         .filter(table.c.geo_code == geo_code) \
         .one()
 
-    return {
-        'name': 'youth',
-        'youth_pop': {
-            "name": "Population (age 15-24)",
-            "values": {"this": youth_pop},
+    final_data = {
+        'lighting_dep': {
+            "name": "Proportion of youth living in households without use of electricity, gas or solar energy for lighting",
+            "values": {"this": float(lighting_dep) or 0.0},
             },
-        'youth_prop': {
-            "name": "Youth (age 15-24) as a percentage of total population",
-            "values": {"this": float(youth_prop) or 0.0},
+        'heating_dep': {
+            "name": "Proportion of youth living in households without use of electricity, gas or solar energy for heating",
+            "values": {"this": float(heating_dep) or 0.0},
             },
-        'edu_dep': {
-            "name": "Deprived in educational attainment *",
-            "values": {"this": float(edu_dep) or 0.0},
-            },
-        'disab_dep': {
-            "name": "Experiencing difficulty in one or more of the following functions: hearing, vision, communication, mobility, cognition and self-care",
-            "values": {"this": float(disab_dep) or 0.0},
-            },
-        'light_dep': {
-            "name": "Living in households without energy for light *",
-            "values": {"this": float(light_dep) or 0.0},
-            },
-        'heat_dep': {
-            "name": "Living in households without energy for heat *",
-            "values": {"this": float(heat_dep) or 0.0},
-            },
-        'cook_dep': {
-            "name": "Living in households without energy for cooking *",
-            "values": {"this": float(cook_dep) or 0.0},
+        'cooking_dep': {
+            "name": "Proportion of youth living in households without use of electricity, gas or solar energy for cooking",
+            "values": {"this": float(cooking_dep) or 0.0},
             },
         'toilet_dep': {
-            "name": "Living in households without a flush toilet",
+            "name": "Proportion of youth living in households without a flush toilet",
             "values": {"this": float(toilet_dep) or 0.0},
             },
         'water_dep': {
-            "name": "Living in households without piped water on site",
+            "name": "Proportion of youth living in households without piped water on site",
             "values": {"this": float(water_dep) or 0.0},
             },
         'dwell_dep': {
-            "name": "Living in households that are informal shacks/traditional dwellings/caravans/tents/other",
+            "name": "Proportion of youth living in households that are informal shacks/traditional dwellings/caravans/tents/other",
             "values": {"this": float(dwell_dep) or 0.0},
             },
         'asset_dep': {
-            "name": "Living in households that do not own more than two of the following 'small' assets: radio, TV, landline, mobile phone, bike, motorbike or refrigerator AND does not own a car or truck",
+            "name": 'Proportion of youth living in households that do not own more than two of the following "small" assets: radio, TV, landline, mobile phone, bike, motorbike or refrigerator AND does not own a car or truck',
             "values": {"this": float(asset_dep) or 0.0},
-            },
+            }
+    }
+
+    return final_data
+
+def get_economic_opportunities_profile(geo_code, geo_level, session):
+    table = get_datatable('youth').model
+
+    (emp_dep, neets_dep, prop_multid_poor, youth_mpi) = session.query(
+            table.c.emp_dep,
+            table.c.neets_dep,
+            table.c.prop_multid_poor,
+            table.c.youth_mpi) \
+        .filter(table.c.geo_level == geo_level) \
+        .filter(table.c.geo_code == geo_code) \
+        .one()
+
+    final_data = {
         'emp_dep': {
-            "name": "Living in households where no working-age adults (age 18-64) are employed ",
+            "name": "Proportion of youth living in households where no working-age adults (age 18-64) are employed",
             "values": {"this": float(emp_dep) or 0.0},
             },
         'neets_dep': {
-            "name": "Not in education, employment or training",
+            "name": "Proportion of youth who are not in education, employment or training",
             "values": {"this": float(neets_dep) or 0.0},
             },
         'prop_multid_poor': {
-            "name": "considered Multidimensionally poor",
+            "name": "Proportion of youth who are multidimensionally poor",
             "values": {"this": float(prop_multid_poor) or 0.0},
             },
         'youth_mpi': {
@@ -208,3 +224,5 @@ def get_depravation_profile(geo_code, geo_level, session):
             "values": {"this": float(youth_mpi) or 0.0},
             }
     }
+
+    return final_data
