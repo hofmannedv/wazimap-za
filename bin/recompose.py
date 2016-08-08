ac2dee8aa8136ae8fc7ad09f4a7e34e5ca057772
Greg Kempe <greg@kempe.net>
2016-08-08 15:22:41 +0200

    Just use pandas. Duh.

--- bin/recompose.py
+++ bin/recompose.py
@@ -1,42 +1,30 @@
 #!/bin/env python
 
-import sys
 import argparse
-import unicodecsv as csv
-from itertools import groupby
+import pandas as pd
+import numpy as np
 
 
 def do_everything(args):
-    # read from first file, index on join key
-    with open(args.first) as f:
-        reader = csv.DictReader(f)
-        first_data = {row[args.join_key]: row for row in list(reader)}
+    first = pd.read_csv(args.first)
+    second = pd.read_csv(args.second)
+    joined = pd.merge(first, second, on=args.join_key)
 
-    # read from second file, group by
-    with open(args.second) as f:
-        reader = csv.DictReader(f)
-        second_data = list(reader)
+    groupby = [args.second_key]
+    if args.groupby:
+        groupby.extend(args.groupby.split(","))
 
-    fields = [fld for fld in first_data.values()[0].keys() if fld != args.join_key]
-    key_func = lambda r: r[args.second_key]
+    # fields to aggregate
+    fields = [f for f in first.columns if f not in groupby and f != args.join_key]
 
-    # process second_data on the intended primary key
-    second_data.sort(key=key_func)
+    # apply factors
+    for field in fields:
+        joined[field] = np.multiply(joined[field], joined[args.factor_key])
 
-    writer = csv.DictWriter(sys.stdout, fieldnames=[args.second_key] + fields)
-    writer.writeheader()
-
-    for key, group in groupby(second_data, key_func):
-        result = {args.second_key: key}
-        for row in group:
-            source = first_data[row[args.join_key]]
-
-            for field in fields:
-                val = source[field]
-                val = float(val) * float(row[args.factor_key])
-                result[field] = result.setdefault(field, 0) + val
-
-        writer.writerow(result)
+    # aggregate
+    joined = joined.groupby(groupby)
+    aggs = joined.agg({f: np.sum for f in fields})
+    print aggs.to_csv()
 
 
 if __name__ == '__main__':
@@ -46,6 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('--second-key', metavar="COLUMN", help='Primary key column name in SECOND file with the factor', required=True)
     parser.add_argument('--first', metavar="FILE", help='CSV file with one row per join attribute', required=True)
     parser.add_argument('--second', metavar="FILE", help='CSV file with factor column')
+    parser.add_argument('--groupby', metavar="COL,COL,...", help='Columns to group by')
     args = parser.parse_args()
 
     do_everything(args)
