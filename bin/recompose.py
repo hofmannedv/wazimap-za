668e0a3aabff76d5fc2a652ffcc5c7b520facafa
Greg Kempe <greg@kempe.net>
2016-08-08 15:46:46 +0200

    Docs

--- bin/recompose.py
+++ bin/recompose.py
@@ -1,4 +1,30 @@
 #!/bin/env python
+#
+# Script to help, eg, recount crimes by ward based on police district crime numbers and estimates
+# of how much of each police station's crimes should be attributed to ward.
+#
+# $ head data/contact-crimes.csv
+# PoliceStation,year,contact,property
+# albertinia,2004,159,93
+# albertinia,2005,110,59
+# albertinia,2006,162,44
+# albertinia,2007,155,37
+# 
+# $ head data/ward-factors.csv
+# WARD_ID,PoliceStation,scal_fac_per_int
+# 10101001,vredendal,0.1247769325578345
+# 10101002,vredendal,0.08020478060328233
+# 10101003,vredendal,0.36114293410747866
+# 10101004,vredendal,0.3437567421111819
+# 10101005,vredendal,0.08819769534894888
+#
+# example: python bin/recompose.py --join-key PoliceStation --second-key WARD_ID --first data/contact-crimes.csv --second data/ward-factors.csv --factor-key scal_fac_per_int  --groupby year
+# WARD_ID,year,property,contact
+# 10101001.0,2004,124.3493023,321.684518997
+# 10101001.0,2005,67.4719876372,299.303478328
+# 10101001.0,2006,70.5007328963,292.565271684
+# ...
+#
 
 import argparse
 import pandas as pd
