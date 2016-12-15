f3b45c167e3f81536837adb3a3d6e12990a58857
Damian <damian@code4sa.org>
2016-12-15 12:37:04 +0200

    Student dropout rates on profile page

--- wazimap_za/profiles/youth.py
+++ wazimap_za/profiles/youth.py
@@ -195,6 +195,17 @@ def get_education_profile(geo_code, geo_level, session, comparative=False):
         key_order={'outcome': ['Bachelor pass', 'No bachelor pass']},
         percent=False, slices=['2015'])
 
+    youth_student_dropout_rate_by_year, _ = get_stat_data(
+        ['year'], geo_level, geo_code, session,
+        table_name='youth_student_dropout_rate_by_year',
+        only={'outcome': ['Dropped out']},
+        percent=False)
+
+    youth_student_dropout_rate_2015, _ = get_stat_data(
+        ['year', 'outcome'], geo_level, geo_code, session,
+        table_name='youth_student_dropout_rate_by_year',
+        percent=False, slices=['2015'])
+
     final_data  = {
         'youth_completed_grade9': youth_completed_grade9,
         'youth_perc_completed_grade9': {
@@ -247,7 +258,12 @@ def get_education_profile(geo_code, geo_level, session, comparative=False):
             "values": {"this": youth_bachelor_outcome_2015['Bachelor pass']['values']['this']}
         },
         'youth_bachelor_outcome_2015': youth_bachelor_outcome_2015,
-        'youth_bachelor_passes_by_year': youth_bachelor_passes_by_year
+        'youth_bachelor_passes_by_year': youth_bachelor_passes_by_year,
+        'youth_student_dropout_rate_2015': {
+            "name": "Of students drop out between grade 10 and matric",
+            "values": {"this": youth_student_dropout_rate_2015['Dropped out']['values']['this']}
+        },
+        'youth_student_dropout_rate_by_year': youth_student_dropout_rate_by_year
     }
 
     return final_data
