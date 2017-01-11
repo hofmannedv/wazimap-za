88c94ddfd39f69cf252fadfcf81959311ac50211
Greg Kempe <greg@kempe.net>
2017-01-11 10:04:51 +0200

    Don't use migrations
    Not quite sure how to tell migrate to use the test db

--- wazimap_za/settings.py
+++ wazimap_za/settings.py
@@ -2,7 +2,7 @@
 from wazimap.settings import *  # noqa
 
 # install this app before Wazimap
-INSTALLED_APPS = ['wazimap_za.apps.WazimapConfig'] + INSTALLED_APPS
+INSTALLED_APPS = ['test_without_migrations', 'wazimap_za.apps.WazimapConfig'] + INSTALLED_APPS
 
 DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://wazimap_za:wazimap_za@localhost/wazimap_za')
 DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
