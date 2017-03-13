f2d1ac10e90385253b2fdc4fb9fc1a3763355789
Damian <damian@code4sa.org>
2017-03-13 11:35:02 +0200

    Add show_video.js

---
+++ wazimap_za/static/js/show_video.js
@@ -0,0 +1,37 @@
+(function($) {
+
+  $(document).ready(function(){
+
+    var pageLinkMap = {
+      'table': 'table_view',
+      'map': 'map_view',
+      'distribution': 'distribution_view',
+    }
+
+    var displayRatio = 0.5625,
+        w = $(window).width() * 0.95,
+        h = w * displayRatio,
+        wMax = 960,
+        hMax = wMax * displayRatio,
+        page = window.location.pathname.split('/')[2],
+        videoLink = VIDEO_LINKS[pageLinkMap[page]];
+
+    if (document.cookie.indexOf('visited=true') === -1) {
+      var expires = new Date();
+      expires.setDate(expires.getDate() + 30);
+      document.cookie = "visited=true; expires=" + expires.toUTCString();
+
+      var showVideo = setTimeout(function () {
+          $.featherlight({
+            iframe: videoLink,
+            iframeMaxWidth: wMax,
+            iframeMaxHeight: hMax,
+            iframeWidth: w,
+            iframeHeight: h
+          });
+        }, 15000);
+    }
+
+  });
+
+}(jQuery));
