f341fc69b93c74b60ddee8b54dcfe335b8084fd9
Greg Kempe <greg@kempe.net>
2017-05-10 16:58:27 +0200

    In-page nav basics

---
+++ wazimap_za/static/js/profiles.js
@@ -0,0 +1,16 @@
+$('.floating-nav').affix({
+  offset: {
+    top: $('.floating-nav').offset().top,
+  },
+});
+
+$('.floating-nav a').on('click', function(e) {
+  e.preventDefault();
+
+  var target = this.hash;
+  var $target = $(target);
+
+  if ($target.length) {
+    window.scroll(0, $target.offset().top - 90);
+  }
+});
