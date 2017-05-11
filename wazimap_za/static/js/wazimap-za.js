425a051dcb689fe769f1c8e63f10a1a46bca255b
Greg Kempe <greg@kempe.net>
2017-05-11 11:42:32 +0200

    Banner

---
+++ wazimap_za/static/js/wazimap-za.js
@@ -0,0 +1,5 @@
+$(function() {
+  // add announcement banner
+  var banner = $('<div id="banner"><div class="wrapper">Wazimap now uses the latest boundaries from the 2016 Municipal Elections. <a href="#">Find out more</a>.</div></div>');
+  $('#page-header').append(banner);
+});
