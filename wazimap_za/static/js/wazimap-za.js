5f1ad83979a1663e0c6bda46f4487fab2ec09005
Greg Kempe <greg@kempe.net>
2017-05-12 12:29:33 +0200

    Article link for update

--- wazimap_za/static/js/wazimap-za.js
+++ wazimap_za/static/js/wazimap-za.js
@@ -1,5 +1,5 @@
 $(function() {
   // add announcement banner
-  var banner = $('<div id="banner"><div class="wrapper">Wazimap now uses the latest boundaries from the 2016 Municipal Elections. <a href="#">Find out more</a>.</div></div>');
+  var banner = $('<div id="banner"><div class="wrapper">Wazimap now uses the latest boundaries from the 2016 Municipal Elections. <a href="https://openup.org.za/articles/wazimap-2016-update.html">Find out more</a>.</div></div>');
   $('#page-header').append(banner);
 });
