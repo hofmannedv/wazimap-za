bb7ade35c6b9d72ab55e02bbe4662c28bbd077d6
Damian <damian@code4sa.org>
2017-12-06 11:08:45 +0200

    Comment to match text

--- wazimap_za/static/js/wazimap-youth.js
+++ wazimap_za/static/js/wazimap-youth.js
@@ -1,5 +1,5 @@
 $(function() {
-  // add banner for pages with Census 2011 data
+  // add banner for pages with data from the latest Community Survey release
   var banner = $('<div id="banner"><div class="wrapper">This page displays data from more than one data release. Unless otherwise noted, data is from the Census 2011</div></div>');
   $('#page-header').append(banner);
 });
